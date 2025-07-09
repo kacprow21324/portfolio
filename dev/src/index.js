9e553f6ca9c02002855a93819ab4350fc7d4dced
kacprow21324 <21324@student.ans-elblag.pl>
2025-07-09 13:32:15 +0200

    Move routing setup to index.js and switch to HashRouter
    Removed router wrapper from App.jsx and wrapped <App /> in HashRouter within src/index.js to eliminate nested routers and enable hash-based routing.

--- dev/src/index.js
+++ dev/src/index.js
@@ -1,16 +1,16 @@
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
