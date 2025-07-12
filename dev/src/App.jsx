5f474d90d10d46e7598e48ee4e935e6ea31657ae
kacprow21324 <21324@student.ans-elblag.pl>
2025-07-12 13:24:12 +0200

    Optimization project

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -5,12 +5,12 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Certificates from './pages/Certificates';
 import Projects from './pages/Projects';
+import './App.css'
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-
       <main className="flex-grow">
         <Routes>
           <Route path="/" element={<Home />} />
